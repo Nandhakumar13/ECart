cd3df2710ee2260c1df1cde2cf329d39db87f8be
Nandhakumar <nandhakumarm1999@gmail.com>
2026-08-23 09:12:42 +0530

    User :: User Model Creation

---
+++ server/model/UserModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator   = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Name is Required Field']
+    },
+    emailId:{
+        type:String,
+        required:[true,'Email ID is required Field'],
+        unique:true,
+        validate:[validator.isEmail, "Enter Valid EmailId"]
+    },
+    password:{
+        type:String,
+        required:[true,'Enter the Password'],
+        minLength: [8, "Password should be have atleast 8 Characters"]
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:'user'
+    },
+    resetPasswordToken:String,
+    resetPasswordTokenExpired:Date,
+    createAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+let schema = mongoose.model('User', userSchema)
+
+module.exports = schema
